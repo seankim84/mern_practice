b455e3eab799d9e847c4b7aafb079a7b312ec392
SeanKim <sean.kim84@gmail.com>
2019-08-06 16:47:21 +0900

    Change the user

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://seankim:zkxkflsk12@ds259347.mlab.com:59347/mern"
+    mongoURI: "mongodb://seankim:zkxkflsk12@ds337377.mlab.com:37377/heroku_6jzr0hf9"
 }
+
+ // mongodb://seankim:zkxkflsk12@ds259347.mlab.com:59347/mern
